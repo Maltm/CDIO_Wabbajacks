7f7ec65874de35f667749e8cff19d03c2ce03f13
Kristin Hansen <maagenator@gmail.com>
2015-04-29 16:34:29 +0200

    fuuuuuuuuuuuuuufufufufuuufuuuuuuUUuUUUuUuuUuuuuUUUUUUUUuuuuuu

--- CDIO_Wabbajacks2/src/camCom/RobotConnection.java
+++ CDIO_Wabbajacks2/src/camCom/RobotConnection.java
@@ -1,42 +1,57 @@
 package camCom;
 
-import java.io.DataInputStream;
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.comm.*;
 
 public class RobotConnection {
-	private DataInputStream in;
+	private BufferedReader in;
 	private DataOutputStream out;
-	
-	// private final String SHEO_ADDRESS = "00:16:53:0A:71:1B";
 	
 	/**
 	 * Class constructor.
 	 */
 	public RobotConnection() {
+		LCD.drawString("Waiting for device...", 0, 0);
+		
 		NXTConnection conn = Bluetooth.waitForConnection();
 		
-		in = conn.openDataInputStream();
-		out = conn.openDataOutputStream();
+		LCD.drawString("Connected to:\n" + conn.getAddress(), 0, 0);
 		
-		String s = null;
+		in = new BufferedReader(new InputStreamReader(conn.openDataInputStream()));
+		out = new DataOutputStream(conn.openDataOutputStream());
+		
+		Button.waitForAnyPress();
 		
 		try {
-			while((s = in.readUTF()) != null) {
-				LCD.drawString(s, 0, 0);
+			LCD.drawString("", 0, 0);
+			
+			out.writeBytes("GO\n");
+			
+			String s = null;
+			int i = 0;
+			
+			while(true) {
+				LCD.drawString(in.readLine(), 0, 0);
 				
 				Button.waitForAnyPress();
 				
-				out.writeBytes("Received: " + s);
+				if(i == 10) break;
+				
+				out.writeBytes(i+"\n");
+				
+				i++;
 			}
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		} finally {
 			try {
+				out.writeBytes("END\n");
 				out.close();
 				in.close();
 			} catch(IOException e) {
