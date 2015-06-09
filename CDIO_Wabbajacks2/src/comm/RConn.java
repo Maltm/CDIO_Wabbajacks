66ffb98be257d24f33236a9e8a91d84eaa18c922
Kristin <Kristin@192.168.1.15>
2015-06-09 20:22:12 +0200

    Comments added to RConn

--- CDIO_Wabbajacks2/src/comm/RConn.java
+++ CDIO_Wabbajacks2/src/comm/RConn.java
@@ -31,50 +31,69 @@ public class RConn {
 	 */
 	public RConn() {
 		while (true) {
+			// Print a string on the display, indicating that RConn has been instantiated
 			LCD.drawString("Waiting for device...", 0, 0);
 			
+			// Open a bluetooth connection and wait for a device to connect
 			NXTConnection conn = Bluetooth.waitForConnection();
 			
+			// When a device has connected print its address on the display 
 			LCD.clear();
 			LCD.drawString("Connected to:\n" + conn.getAddress(), 0, 0);
 			
+			// Open streams for communication
 			in = new BufferedReader(new InputStreamReader(conn.openDataInputStream()));
 			out = conn.openDataOutputStream();
 			
 			try {
+				// Print a "ready"-message on the display, indicating that the brick is ready for commands
 				LCD.clear();
 				LCD.drawString("Ready for commands.", 0, 0);
 				
+				// Write to the connected device that the brick is ready
 				out.writeBytes("READY\n");
 				out.flush();
 				
+				// Buffer string
 				String msg = null;
 				
 				while(true) {
+					// If the string is anything but NULL...
 					if((msg = in.readLine()) != null) {
-						if(msg.equals("END")) break;
+						// ... and NOT "END"
+						if(msg.equals("END")) break;						
 						
+						// ... print received command
+						// TODO If a series of commands are received don't print them all (maybe print the amount of commands?)
 						LCD.clear();
 						LCD.drawString("RConn received:\n" + msg, 0, 0);
 						
+						// ... send the command to the robot controller and prepare a response
 						String response = rctrl.doCommand(msg);
 						
+						// Write the response back to the connected device
 						out.writeBytes(response + "\n");
 						out.flush();
 						
+						// Print the response on the display
 						LCD.clear();
 						LCD.drawString("RConn sending:\n" + response, 0, 0);
 					}
 				}
 			} catch (IOException e) {
+				// Print exception message
 				System.out.println(e.getMessage());
 			} finally {
+				// End communication and close ressources
 				try {
+					// Write an ending message to the connected device
 					out.writeBytes("END\n");
 					out.flush();
 					
+					// Close resources
 					out.close();
 					in.close();
+					conn.close();
 				} catch(IOException e) {
 					System.out.println(e.getMessage());
 				}
@@ -83,6 +102,7 @@ public class RConn {
 	}
 	
 	public static void main(String[] args) {
+		// Start connection
 		new RConn();
 	}
 }
